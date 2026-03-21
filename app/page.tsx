9f4ed7a45cc48b8077425057ce23eb17792036cc
cleilsonnog <cleilsonnogueira45@gmail.com>
2026-03-21 13:47:36 -0300

    chore: setup da pagina de login

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,28 @@
 import Image from "next/image";
+import { redirect } from "next/navigation";
+import dayjs from "dayjs"; 
+import { authClient } from "./_lib/auth-client";
+import { headers } from "next/headers";
+import { getHome, getUserTrainData } from "./_lib/api/fetch-generated";
 
-export default function Home() {
+export default async function Home() {
+  const session = await authClient.getSession({
+    fetchOptions: {
+       headers: await headers(),
+    },
+  });
+  if (!session.data?.user) redirect("/auth");
+
+  const today = dayjs();
+  const [homeData, trainData] = await Promise.all([
+    getHome(today.format("YYYY-MM-DD")),
+    getUserTrainData(),
+  ]);
+console.log(homeData);
+
+  if (homeData.status !== 200) {
+    throw new Error("Failed to fetch home data");
+  }
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
